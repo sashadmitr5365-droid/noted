f648395c28632d37d4fcee84c0aa4c2146814307
sashadmitr5365-droid <sashadmitr5365@gmail.com>
2026-07-29 19:46:02 +0300

    Delete src directory

--- src/app/settings/page.tsx
+++
@@ -1,8 +0,0 @@
-import SettingsScreen from "@/components/SettingsScreen";
-
-export const dynamic = "force-dynamic";
-export const runtime = "nodejs";
-
-export default function SettingsPage() {
-  return <SettingsScreen />;
-}
